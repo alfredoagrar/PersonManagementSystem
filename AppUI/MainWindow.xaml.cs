fbc0f4e1ad0736baf5fdbf995796e99d910fb810
Alfredo Aguiar Arce <165372549+alfredoagrar@users.noreply.github.com>
2024-04-03 17:49:07 -0700

    :construction: UI Services HTTP

--- AppUI/MainWindow.xaml.cs
+++ AppUI/MainWindow.xaml.cs
@@ -1,24 +1,39 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System;
+using AppUI.Services.Interfaces;
 
 namespace AppUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IHttpClientService _httpClientService;
+
+        // Constructor sin parámetros
+        // Constructor sin parámetros
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public MainWindow(IHttpClientService httpClientService) : this()
+        {
+            InitializeComponent();
+            _httpClientService = httpClientService;
+            CallApi();
+        }
+
+        // Método para llamar a la API, asegúrate de llamar a este método de manera adecuada en tu UI.
+        private async void CallApi()
+        {
+            try
+            {
+                var result = await _httpClientService.GetAsync<List<Persona>>("https://localhost:7231/directorio/personas");
+                // Aquí manejarías el resultado, por ejemplo, mostrándolo en la UI.
+            }
+            catch (Exception ex)
+            {
+                // Manejar la excepción, por ejemplo, mostrando un mensaje al usuario.
+            }
         }
     }
 }
---
+++ AppUI/Utils/Interfaces/IHttpClientService.cs
@@ -0,0 +1,12 @@
+﻿namespace AppUI.Services.Interfaces
+{
+    internal abstract class HttpClientBase : IHttpClientService
+    {
+        public abstract Task<T> GetAsync<T>(string uri);
+    }
+    public interface IHttpClientService
+    {
+        Task<T> GetAsync<T>(string uri);
+    }
+
+}
